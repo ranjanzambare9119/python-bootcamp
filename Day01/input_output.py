65351ee78138e8d3bf39a0f5163eb7858ce6b03b
Ranjan Zambare <ranjanzambare9119@gmail.com>
2026-07-09 22:57:43 +0530

    Day 1: Variables, Input Output and Information Card Programs

---
+++ Day01/input_output.py
@@ -0,0 +1,23 @@
+# Personal Details
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+
+print("\n----- Personal Details -----")
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+
+print()
+
+# Favourite Details
+
+restaurant_name = input("Enter your favourite restaurant: ")
+food = input("Enter your favourite food: ")
+programming_language = input("Enter your favourite programming language: ")
+
+print("\n----- Favourite Details -----")
+print("Restaurant:", restaurant_name)
+print("Favourite Food:", food)
+print("Programming Language:", programming_language)
---
+++ Day01/variables.py
@@ -0,0 +1,31 @@
+# Student Details
+
+student_name = "Ranjan Zambare"
+age = 20
+college_name = "KSE"
+branch = "AI & DS"
+cgpa = 8.9
+
+print("----- Student Details -----")
+print("Student Name:", student_name)
+print("Age:", age)
+print("College:", college_name)
+print("Branch:", branch)
+print("CGPA:", cgpa)
+
+print()
+
+# Restaurant Details
+
+restaurant_name = "Sugee Veg"
+owner_name = "Sharad"
+number_of_employees = 12
+average_daily_customers = 50
+is_pure_veg = True
+
+print("----- Restaurant Details -----")
+print("Restaurant Name:", restaurant_name)
+print("Owner Name:", owner_name)
+print("Number of Employees:", number_of_employees)
+print("Average Daily Customers:", average_daily_customers)
+print("Pure Veg:", is_pure_veg)
