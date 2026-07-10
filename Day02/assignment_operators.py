415875d6245b103908a827594048e35719dd622f
Ranjan Zambare <ranjanzambare9119@gmail.com>
2026-07-10 20:16:02 +0530

    Day 2: Type conversion, operators and calculator programs

---
+++ Day02/assignment_operators.py
@@ -0,0 +1,12 @@
+marks=50
+print('==========')
+print('Assignment Operators')
+print('==========')
+marks +=50
+print('Adding 50 to marks:', marks)
+marks -=20
+print('Subtracting 20 from marks:', marks)
+marks *= 2
+print('Multiplying the marks by 2:', marks)
+marks /= 2
+print('Dividing the marks by 2:', marks)
---
+++ Day02/type_coversion.py
@@ -0,0 +1,4 @@
+age= input('What is your age:')
+print(type(age))
+age= int(input('What is your age:'))
+print(type(age))
